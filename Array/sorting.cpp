a38171213e621ec87edda998bb7280ff2b71d0b4
mdsagil <sagilzufequar@gmail.com>
2026-08-24 21:25:51 +0530

    sorting problem

---
+++ Array/sorting.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int nums[], int n) {
+
+    for(int i = 0; i < n-1; i++) {
+
+        int minIndex = i;
+
+        for(int j = i+1; j < n; j++) {
+
+            if(nums[j] < nums[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        swap(nums[i], nums[minIndex]);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int nums[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    selectionSort(nums, n);
+
+    for(int i = 0; i < n; i++) {
+        cout << nums[i] << " ";
+    }
+}
